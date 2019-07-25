4372a9a352091772338c4a2ab345be6ddaad941d
Rayim <tonite32@gmail.com>
2019-07-25 18:08:59 +0900

    update logic

--- contracts/NestreeChannelDrop.sol
+++ contracts/NestreeChannelDrop.sol
@@ -17,24 +17,26 @@ contract NestreeChannelDrop is Ownable
 
     string constant internal ERROR_VALUE_NOT_VALID             = 'Reason: Value must be greater than 0.';
     string constant internal ERROR_BALANCE_NOT_ENOUGH          = 'Reason: Balance is not enough.';
+    string constant internal ERROR_TOKEN_ADDRESS_NOT_VALID     = 'Reason: Token address is not valid.';
+    string constant internal ERROR_NOT_VALID_RECEIVER_LIST     = 'Reason: Receiver list is not valid.';
 
     address public _owner;
 
     mapping (address => address) private channelMasters;
     mapping (address => address) private proposedChannelMasters;
-    mapping (address => mapping(address => uint256)) balances;
+    mapping (address => mapping(address => uint256)) balances; // channelAddress => tokenAddress => balance
 
     event TransferChannelMaster(address indexed previousMaster, address indexed newMaster);
     event Deposit(address indexed sender, address indexed channelAddress, address indexed tokenAddress, uint256 amount);
     event Withdraw(address indexed channelAddress, address indexed receiver, uint256 amount);
-    event Drop(address _tokenAddress, address[] _toList, uint256[] _amountList);
+    event Drop(address tokenAddress, address[] toList, uint256[] amountList, uint256 _type);
 
     constructor() public
     {
         _owner = msg.sender;
     }
 
-    function isMaster(address _channelAddress, address _target) external returns (bool)
+    function isMaster(address _channelAddress, address _target) external view returns (bool)
     {
         return channelMasters[_channelAddress] == _target;
     }
@@ -91,46 +93,48 @@ contract NestreeChannelDrop is Ownable
     {
         require(_from != address(0), ERROR_ADDRESS_NOT_VALID);
         require(_tokenAddress != address(0), ERROR_ADDRESS_NOT_VALID);
-        require(amount != 0, ERROR_VALUE_NOT_VALID);
+        require(_amount != 0, ERROR_VALUE_NOT_VALID);
         require(channelMasters[_from] == msg.sender, ERROR_NO_HAVE_PERMISSION);
 
         ERC20 token = ERC20(_tokenAddress);
-        require(balances[_from][_tokenAddress] >= amount);
-        token.transfer(_to, amount);
-        balances[_from][_tokenAddress] = balances[_from][_tokenAddress].sub(amount);
+        require(token.balanceOf(address(this)) >= _amount); // 컨트랙트 전체 보유 토큰 수량과 비교
+        require(balances[_from][_tokenAddress] >= _amount); // 채널 지갑 보유 토큰 수량과 비교
+        token.transfer(_to, _amount);
+        balances[_from][_tokenAddress] = balances[_from][_tokenAddress].sub(_amount);
 
-        emit Withdraw(_from, _to, amount);
+        emit Withdraw(_from, _to, _amount);
     }
 
-    function balanceOf(address _tokenAddress, address _channelAddress) public view returns (uint256)
+    function balanceOf(address _channelAddress, address _tokenAddress) public view returns (uint256)
     {
-        ERC20 token = ERC20(_tokenAddress);
-        return token.balanceOf(address(this));
+        return balances[_channelAddress][_tokenAddress];
     }
 
-    function drop(address _tokenAddress, address[] calldata _toList, uint256[] calldata _amountList) external
+    function drop(address _tokenAddress, address[] calldata _toList, uint256[] calldata _amountList, uint256 _type) external
     {
-        require(_tokenAddress != address(0), 'Token address is not valid');
-        require(_toList.length == _amountList.length, 'Not valid list length');
+        require(_tokenAddress != address(0), ERROR_TOKEN_ADDRESS_NOT_VALID);
+        require(_toList.length == _amountList.length, ERROR_NOT_VALID_RECEIVER_LIST);
 
-        ERC20 token = ERC20(_tokenAddress);
-
-        uint256 sumOfBalances = 0;
+        uint256 sumOfAmounts = 0;
         for(uint256 i=0; i<_amountList.length; i++)
         {
-            sumOfBalances = sumOfBalances.add(_amountList[i]);
+            require(_toList[i] != address(0), ERROR_ADDRESS_NOT_VALID);
+            require(_amountList[i] != 0, ERROR_VALUE_NOT_VALID);
+
+            sumOfAmounts = sumOfAmounts.add(_amountList[i]);
         }
 
         // 합계가 발란스보다 많은지 체크
-        uint256 balance = token.allowance(msg.sender, _self);
+        ERC20 token = ERC20(_tokenAddress);
 
-        require(balance >= sumOfBalances);
+        require(token.balanceOf(address(this)) >= sumOfAmounts); // 컨트랙트 전체 보유 토큰 수량과 비교
+        require(balances[msg.sender][_tokenAddress] >= sumOfAmounts); // 채널 지갑 보유 토큰 수량과 비교
 
         for(uint256 i=0; i<_toList.length; i++)
         {
-            token.transferFrom(msg.sender, _toList[i], _amountList[i]);
+            token.transfer(_toList[i], _amountList[i]);
         }
 
-        emit Drop(_tokenAddress, _toList, _amountList);
+        emit Drop(_tokenAddress, _toList, _amountList, _type);
     }
 }
